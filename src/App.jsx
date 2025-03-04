72b7f7a1e66a149599fb717c4bef315010799780
Tiolulope <juliusdedeji01@gmail.com>
2025-03-04 19:28:39 +0100

    my movie app

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import React, {useEffect, useState} from 'react'
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
+import search from "./components/Search.jsx";
+import {useDebounce} from "react-use";
+import {updateSearchCount} from "./appwrite.js";
+
+
+
+
+//API - Application Programming Interface - A set of rules that allow one software application to talk to another.
+
+const API_BASE_URL = "https://api.themoviedb.org/3"
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
+
+const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const [errorMessage,setErrorMessage] = useState('')
+    const [movieList, setMovieList] = useState([]);
+    const [trendingMovies   , setTrendingMovies   ] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
+    const [deBounceSearchTerm  , setDeBounceSearchTerm  ] = useState('')
+
+
+
+    //Debounce the search term to prevent making too many API requests
+    //by waiting for the user to stop typing for 500ms
+    useDebounce(() => setDeBounceSearchTerm(searchTerm), 500,[searchTerm])
+
+    const fetchMovies = async (query = "") => {
+
+        const loadTrendingMovies = async () => {
+            try {
+
+            }catch (error) {
+                console.log(`Error fetching trending movies${error}`)
+                setErrorMessage('Error fetching trending movies.')
+            }
+        }
+
+        setIsLoading(true);
+        setErrorMessage('');
+        try{
+            const endpoint = query ?
+                `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if (!response.ok){
+                throw new Error("Could not find movies details")
+            }
+
+           const data = await response.json();
+
+            if (data.response === "False"){
+                setErrorMessage(data.Error || "failed to fetch movies");
+                setMovieList([]);
+                return;
+            }
+
+            setMovieList(data.results || []);
+
+            if(query && data.results.length > 0) {
+               await updateSearchCount(query,data.results[0]);
+            }
+
+
+        } catch(error) {
+            console.log(`Error fetching movies: ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+            fetchMovies(searchTerm);
+    }, [searchTerm]);
+
+    return (
+        <main>
+            <div className="pattern" />
+                <div className="wrapper">
+                    <header>
+                        <img src="/hero-img.png " alt="Hero Banner"/>
+                        <h1>Find <span className="text-gradient">Movies</span> you will enjoy without the hassle</h1>
+                        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                    </header>
+                      <section className="all-movies">
+                            <h2 className="mt-[40px]">All Movies</h2>
+                          {isLoading ? (
+                              <Spinner />
+                          ): errorMessage ? (
+                              <p className = "text-red-500"> {errorMessage} </p>
+                          ): (
+                              <ul>
+                                  {movieList.map((movie) => (
+                                     <MovieCard key={movie.id} movie={movie} />
+                                  ))}
+                              </ul>
+                          )}
+                      </section>
+                </div>
+
+        </main>
+    )
+}
+export default App
